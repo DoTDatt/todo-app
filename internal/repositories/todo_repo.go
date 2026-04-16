3b502a8102be17f87eda335df4be3c8c1f9b6112
ThanhDat <panhhuni@gmail.com>
2026-04-16 10:58:05 +0700

    add Delete By ID in handler and repo

--- internal/repositories/todo_repo.go
+++ internal/repositories/todo_repo.go
@@ -32,3 +32,14 @@ func (r *TodoRepository) GetbyID(id uint) (*models.Todo, error) {
 func (r *TodoRepository) Update(todo *models.Todo) error {
 	return r.db.Updates(todo).Error
 }
+
+func (r *TodoRepository) GetByID(id uint) (*models.Todo, error) {
+	var todo models.Todo
+	err := r.db.First(&todo, id).Error
+	return &todo, err
+}
+
+func (r *TodoRepository) Delete(id uint) error {
+	var todo models.Todo
+	return r.db.Delete(&todo, id).Error
+}
