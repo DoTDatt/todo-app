895650e71373533bd8ad41ef314939c4722227a2
ThanhDat <panhhuni@gmail.com>
2026-04-20 23:16:09 +0700

    feat: create user and role models , add user field to todo.go

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type Todo struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`
+	ID          int       `json:"id" gorm:"primaryKey"`
 	Title       string    `json:"title" gorm:"type:varchar(255);not null"`
 	Description string    `json:"description" gorm:"type:text"`
 	Status      string    `json:"status" gorm:"type:enum('pending','in_progress','done');default:'pending'"`
+	UserID      int       `json:"user_id" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
